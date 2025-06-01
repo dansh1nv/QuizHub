689aec32c628c503aad324181d78be577ea9d351
danshin.vladimir <danshin.vladimir@wb.ru>
2025-06-01 17:25:24 +0300

    add edgeToEdge

--- app/src/main/java/ru/dansh1nv/quizhub/MainActivity.kt
+++ app/src/main/java/ru/dansh1nv/quizhub/MainActivity.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -31,6 +34,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        enableEdgeToEdge()
         setContent { QuizHubApp() }
         observerGlobalEvents()
     }
@@ -69,7 +74,9 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
             QuizHubTheme(isDarkTheme = true) {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding(),
                     color = QuizHubTheme.colorScheme.surface
                 ) {
                     AppNavGraph(
