eaff9b028da5375018c897bef57a289d11fd9047
danshin.vladimir <danshin.vladimir@wb.ru>
2025-06-22 17:30:06 +0300

    add new base components and update design CityBottomSheet

--- features/quiz-list/src/main/java/ru/dansh1nv/quiz/list/presentation/composable/bottomSheets/CityBottomSheet.kt
+++ features/quiz-list/src/main/java/ru/dansh1nv/quiz/list/presentation/composable/bottomSheets/CityBottomSheet.kt
@@ -1,32 +1,43 @@
 package ru.dansh1nv.quiz.list.presentation.composable.bottomSheets
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import ru.dansh1nv.designsystem.theme.elements.QuizHubButton
+import ru.dansh1nv.designsystem.theme.elements.QuizHubRadioButton
 import ru.dansh1nv.designsystem.theme.elements.search.CustomSearchBar
 import ru.dansh1nv.designsystem.theme.uiKit.QuizHubTheme
 import ru.dansh1nv.quiz.list.models.CityModel
 import ru.dansh1nv.quiz.list.presentation.QuizListEvent
 import ru.dansh1nv.quiz.list.presentation.QuizListState
 import ru.dansh1nv.quiz.list.presentation.ScreenEvent
+import ru.dansh1nv.designsystem.R.string as UIString
 
 @Composable
 internal fun CityBottomSheet(
@@ -34,45 +45,73 @@ internal fun CityBottomSheet(
     onUIEvent: (QuizListEvent) -> Unit,
 ) {
 
+    var footerHeight by remember { mutableIntStateOf(0) }
+    val footerHeightToDp by remember { mutableStateOf(footerHeight.dp + 12.dp) }
     var searchQuery by remember { mutableStateOf("") }
     val searchResults by remember(searchQuery) {
         derivedStateOf { screenState.cities.filter { it.isSearchVisible } }
     }
+    val localSelectedCity = remember { mutableStateOf(screenState.currentCity) }
 
-    Column(
+    Box(
         modifier = Modifier
-            .fillMaxWidth()
+            .fillMaxSize()
             .padding(horizontal = 16.dp, vertical = 8.dp)
     ) {
-        val keyboardController = LocalSoftwareKeyboardController.current
-        val focusRequester = remember { FocusRequester() }
-        val focusManager = LocalFocusManager.current
+        Column(modifier = Modifier.fillMaxSize()) {
+            val keyboardController = LocalSoftwareKeyboardController.current
+            val focusRequester = remember { FocusRequester() }
+            val focusManager = LocalFocusManager.current
 
-        CustomSearchBar(
-            query = searchQuery,
-            onQueryChange = {
-                searchQuery = it
-                onUIEvent(ScreenEvent.OnSearch(searchQuery))
-            },
-            onClear = {
-                searchQuery = ""
-                onUIEvent(ScreenEvent.OnSearch(searchQuery))
-            },
-            keyboardController = keyboardController,
-            focusRequester = focusRequester,
-            focusManager = focusManager,
-        )
+            CustomSearchBar(
+                query = searchQuery,
+                onQueryChange = {
+                    searchQuery = it
+                    onUIEvent(ScreenEvent.OnSearch(searchQuery))
+                },
+                onClear = {
+                    searchQuery = ""
+                    onUIEvent(ScreenEvent.OnSearch(searchQuery))
+                },
+                keyboardController = keyboardController,
+                focusRequester = focusRequester,
+                focusManager = focusManager,
+            )
 
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(16.dp)
-        ) {
-            items(searchResults) { item ->
-                CityItem(
-                    item = item,
-                    onUIEvent = onUIEvent,
-                )
+            LazyColumn(
+                modifier = Modifier
+                    .padding(bottom = footerHeightToDp)
+                    .fillMaxSize(),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                itemsIndexed(searchResults) { index, item ->
+                    CityItem(
+                        item = item,
+                        localSelectedCity = localSelectedCity,
+                    )
+
+                    if (searchResults.lastIndex != index) {
+                        HorizontalDivider(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 4.dp),
+                            thickness = 1.dp,
+                            color = QuizHubTheme.colorScheme.onSurface,
+                        )
+                    }
+                }
             }
+        }
+
+        Box(modifier = Modifier.align(Alignment.BottomCenter)) {
+            QuizHubButton(
+                title = stringResource(UIString.select_button_text),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .onSizeChanged { footerHeight = it.height },
+                isEnabled = localSelectedCity.value != screenState.currentCity,
+                onClick = { onUIEvent(ScreenEvent.OnCityClick(localSelectedCity.value)) },
+            )
         }
     }
 }
@@ -80,27 +119,24 @@ internal fun CityBottomSheet(
 @Composable
 private fun CityItem(
     item: CityModel,
-    onUIEvent: (QuizListEvent) -> Unit,
+    localSelectedCity: MutableState<CityModel>,
 ) {
-    Column(
+    Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(all = 8.dp)
-            .clickable { onUIEvent(ScreenEvent.OnCityClick(item)) }
+            .clickable { localSelectedCity.value = item },
+        horizontalArrangement = Arrangement.SpaceBetween,
     ) {
         Text(
             text = item.name,
             style = QuizHubTheme.typography.bodyLarge,
             color = QuizHubTheme.colorScheme.onSurface,
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        HorizontalDivider(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 4.dp),
-            thickness = 1.dp,
-            color = QuizHubTheme.colorScheme.onSurface,
+                .align(Alignment.CenterVertically),
         )
+
+        QuizHubRadioButton(isSelected = localSelectedCity.value == item)
     }
 }
