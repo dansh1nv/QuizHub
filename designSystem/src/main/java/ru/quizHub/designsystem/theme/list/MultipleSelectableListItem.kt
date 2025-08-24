70e47e737e64e7d9c01aecaac33edd342577a20c
danshin.vladimir <danshin.vladimir@wb.ru>
2025-08-24 19:36:01 +0300

    update MultipleSelectableListItem component

--- designSystem/src/main/java/ru/quizHub/designsystem/theme/list/MultipleSelectableListItem.kt
+++ designSystem/src/main/java/ru/quizHub/designsystem/theme/list/MultipleSelectableListItem.kt
@@ -1,49 +1,52 @@
 package ru.quizHub.designsystem.theme.list
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import ru.quizHub.designsystem.theme.elements.QuizHubCheckBox
 import ru.quizHub.designsystem.theme.uiKit.QuizHubTheme
 
 @Composable
-fun <Item> MultipleSelectableListItem(
+fun MultipleSelectableListItem(
     text: String,
     isSelected: Boolean,
-    item: Item,
-    localState: MutableState<Item>,
+    modifier: Modifier = Modifier,
     onClick: (Boolean) -> Unit = {},
 ) {
+    val (checkedState, onStateChange) = remember { mutableStateOf(isSelected) }
     Row(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
-            .padding(8.dp)
-            .clickable {
-                localState.value = item
-                onClick.invoke(!isSelected)
-            },
+            .toggleable(
+                value = checkedState,
+                onValueChange = {
+                    onStateChange(!checkedState)
+                    onClick(checkedState)
+                },
+                role = Role.Checkbox
+            )
+            .padding(vertical = 12.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
     ) {
         Text(
             text = text,
             style = QuizHubTheme.typography.bodyLarge,
             color = QuizHubTheme.colorScheme.onSurface,
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.CenterVertically),
+            modifier = Modifier.wrapContentWidth(),
         )
 
-        QuizHubCheckBox(
-            isSelected = localState.value == item,
-            onClick = onClick,
-        )
+        QuizHubCheckBox(isSelected = checkedState)
     }
 }
